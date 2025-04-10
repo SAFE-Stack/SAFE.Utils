175a541a95b97c1bf1b0d0cf3b9844ed956ed59c
Joost Kaptein <joost@compositional-it.com>
2025-04-10 14:40:11 +0100

    set up for separate versions

--- Build/Program.fs
+++ Build/Program.fs
@@ -3,9 +3,40 @@ open Fake.Core.TargetOperators
 open Fake.IO
 open Fake.JavaScript
 
+type Project =
+    | Server
+    | Client
+
+    static member fromString input =
+        match input with
+        | "client" -> Client
+        | "server" -> Server
+        | other -> failwith $"unknown project {other}"
+
+    member this.Name =
+        match this with
+        | Server -> "Server"
+        | Client -> "Client"
+
+    member this.sourceFolder = $"../src/{this.Name}"
 
 let execContext = Context.FakeExecutionContext.Create false "build.fsx" []
 Context.setExecutionContext (Context.RuntimeContext.Fake execContext)
+
+
+module Environment =
+
+    let projectAndVersion () =
+        let versionComponents = (Environment.environVarOrFail "PROJECT_VERSION").Split '_'
+
+        let project = versionComponents[0] |> Project.fromString
+        let version = versionComponents[1]
+
+        project, version
+
+    let releaseNotes () =
+        Environment.environVarOrFail "RELEASE_NOTES_URL"
+
 
 module Processes =
     let createProcess exe args dir =
@@ -22,12 +53,10 @@ module Processes =
 
     let runDotnet = run dotnet
 
-let sourceFolder = Path.getFullName """../src"""
 let outputFolder = Path.getFullName """../nugetPackages"""
 
 let clientTestFolder = Path.getFullName """../test/Client"""
 
-let projects = [ "Client"; "SAFE.Server" ]
 
 Target.create "Test" (fun _ ->
     Npm.install (fun o ->
@@ -39,19 +68,18 @@ Target.create "Test" (fun _ ->
             WorkingDirectory = clientTestFolder }))
 
 Target.create "Bundle" (fun _ ->
-    let version = Environment.environVarOrFail "VERSION"
-    let releaseNotes = Environment.environVarOrFail "RELEASE_NOTES_URL"
 
-    projects
-    |> List.map (fun project ->
-        Processes.runDotnet
-            [ "pack"
-              "-o"
-              outputFolder
-              $"-p:PackageVersion={version}"
-              $"-p:PackageReleaseNotes={releaseNotes}" ]
-            $"""{sourceFolder}/{project}""")
-    |> ignore)
+    let releaseNotes = Environment.releaseNotes
+
+    let project, version = Environment.projectAndVersion ()
+
+    Processes.runDotnet
+        [ "pack"
+          "-o"
+          outputFolder
+          $"-p:PackageVersion={version}"
+          $"-p:PackageReleaseNotes={releaseNotes}" ]
+        project.sourceFolder)
 
 
 Target.create "Publish" (fun _ ->
